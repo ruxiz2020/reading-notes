af2df9958db9e6cdf39ef311c8bdb3b620a26f41
zruxi <zruxi@vmware.com>
2020-08-18 17:46:52 -0700

    archiving assets

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='templates')
+
 
 @app.route("/")
 def index():
-        #return("Index str")
     return render_template("index.html")
+
+
+@app.route("/all-tags")
+def alltags():
+    return render_template("/all-tags/index.html")
+
+
+@app.route("/all-categories")
+def allcategories():
+    return render_template("/all-categories/index.html")
+
+
+@app.route("/all-archives")
+def allarchives():
+    return render_template("/all-archives/index.html")
+
+
+@app.route('/<year>/<month>/<day>/<blogname>/')
+def archives(year, month, day, blogname):
+
+    pth = "/" + year + "/" + month + \
+        "/" + day + "/" + blogname
+
+    print(pth)
+    return render_template(pth + "/index.html")
+
 
 if __name__ == "__main__":
     app.run()
